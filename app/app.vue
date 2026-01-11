2cd0c1f99b08b9565602396bfeb09d7a68d9e6f2
Olga Voronenko <olga.voronenko@tptlive.ee>
2026-01-11 17:39:02 +0200

    add nuxtui, init nuxt project

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <UApp>
+     <UHeader title="Nuxt UI" >
+     <template #right>
+      <UColorModeButton />
+     </template>
+     </UHeader>
+    <NuxtPage></NuxtPage>
+  </UApp>
+</template>
